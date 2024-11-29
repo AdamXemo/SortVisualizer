5bcc835d7228c6dfdcebcc77e82968a9c46e0a08
lyzska <>
2024-11-29 20:57:46 +0000

    implement merge sort

--- sort.py
+++ sort.py
@@ -15,6 +15,26 @@ def insertion_sort(arr):
             yield arr
             j -= 1
 
+def merge(arr1, arr2):
+    if arr1 == []:
+        return arr2
+    elif arr2 == []:
+        return arr1
+    elif arr1[0] < arr2[0]:
+        return [arr1[0]] + merge(arr1[1:], arr2)
+    else:
+        return [arr2[0]] + merge(arr1, arr2[1:])
+
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+
+    middle = len(arr) // 2
+
+    return merge(
+        merge_sort(arr[:middle]),
+        merge_sort(arr[middle:]))
+
 if __name__ == "__main__":
     import random
 
