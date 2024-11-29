213173d0b79ec4c8817f3a28f22cf7534eef674a
Adam Kich <adam.kich@student.ucll.be>
2024-11-29 18:49:40 +0100

    Add insertion sort

--- sort.py
+++ sort.py
@@ -6,11 +6,21 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
 if __name__ == "__main__":
     import random
 
     arr = random.sample(range(10), 10)
     print(arr)
 
-    sorted_arr = bubble_sort(arr)
+    sorted_arr = insertion_sort(arr)
     print(sorted_arr)
