1ac159dd0fd017167f0ee03abb7a26d7c1e63692
Adam Kich <adam.kich@student.ucll.be>
2024-11-30 20:11:22 +0100

    add variable to stop playing sounds, do that when finished

--- visualize.py
+++ visualize.py
@@ -44,7 +44,7 @@ def play_sine_wave(frequency):
     sound.play(0)
 
 
-def draw_array(array, highligh_color = RED):
+def draw_array(array, highligh_color, play_sounds):
     screen.fill((0, 0, 0))
     for i in range(len(array["values"])):
         draw_element(array["values"], i, GRAY)
@@ -52,8 +52,10 @@ def draw_array(array, highligh_color = RED):
         draw_element(array["values"], i, highligh_color)
 
     last_highlighted = array["values"][array["highlight"][-1]]
-    frequency = 220 * 4**((last_highlighted-1) / ARRAY_SIZE)
-    play_sine_wave(frequency)
+
+    if play_sounds:
+        frequency = 220 * 4**((last_highlighted-1) / ARRAY_SIZE)
+        play_sine_wave(frequency)
 
 def draw_text(text):
     font = pygame.font.SysFont("Arial", 40)
@@ -69,9 +71,9 @@ paused = False
 sorting_steps = list(sort.merge_sort(random_array))
 current_step_index = 0
 highlighted_index = 1
+play_sounds = True
 
 while True:
-    redraw = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -83,11 +85,9 @@ while True:
             if event.key == pygame.K_RIGHT:  # step forward
                 if current_step_index < len(sorting_steps) - 1:
                     current_step_index += 1
-                    redraw = True
             if event.key == pygame.K_LEFT:  # step backward
                 if current_step_index > 0:
                     current_step_index -= 1
-                    redraw = True
             if event.key == pygame.K_r:
                 random_array = random.sample(range(1, ARRAY_SIZE+1), ARRAY_SIZE)
                 sorting_steps = list(sort.bubble_sort(random_array))
@@ -100,28 +100,25 @@ while True:
     if keys[pygame.K_d]:
         if current_step_index < len(sorting_steps) - 1:
             current_step_index += 1
-            redraw = True
     if keys[pygame.K_a]:
         if current_step_index > 0:
             current_step_index -= 1
-            redraw = True
 
     if not paused and current_step_index < len(sorting_steps) - 1:
         current_step_index += 1
-        redraw = True
 
     if current_step_index == len(sorting_steps) - 1:
         if highlighted_index < len(random_array) - 1:
             highlighted_index += 1
             time.sleep(0.01)
-        draw_array({"values": sorting_steps[current_step_index]["values"], "highlight": [highlighted_index]}, GREEN)
+        draw_array({"values": sorting_steps[current_step_index]["values"], "highlight": [highlighted_index]}, GREEN, play_sounds)
 
         if highlighted_index == len(random_array) - 1:
-            draw_array({"values": sorting_steps[current_step_index]["values"], "highlight": [highlighted_index]}, GRAY)
+            play_sounds = False
+            draw_array({"values": sorting_steps[current_step_index]["values"], "highlight": [highlighted_index]}, GRAY, play_sounds)
             draw_text("Sorting Complete! Press R to Reset, Q to quit.")
-
-    elif redraw:
-        draw_array(sorting_steps[current_step_index])
+    else:
+        draw_array(sorting_steps[current_step_index], RED, play_sounds)
 
     if paused:
         draw_text("Sorting Paused. Press Space to Resume.")
